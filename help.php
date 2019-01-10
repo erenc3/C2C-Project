648e6be49840a9139f53af41b5a3f5c46b6ca563
erenc3 <34813034+erenc3@users.noreply.github.com>
2019-01-11 01:26:30 +0300

    79 - Yardım Sayfası

---
+++ help.php
@@ -0,0 +1,133 @@
+﻿<?php require_once 'header.php' ?>
+            <!-- Main Banner 1 Area Start Here -->
+            <?php require_once 'search.php' ?>
+            <!-- Main Banner 1 Area End Here --> 
+            <!-- Inner Page Banner Area Start Here -->
+            <div class="pagination-area bg-secondary">
+                <div class="container">
+                    <div class="pagination-wrapper">
+                        
+                    </div>
+                </div>  
+            </div> 
+            <!-- Inner Page Banner Area End Here -->          
+            <!-- Help Page Start Here -->
+            <div class="help-page-area bg-secondary section-space-bottom">
+                <div class="container">
+                    <h2 class="title-section">Nasıl Yardımcı Olabiliriz?</h2>
+                    <div class="inner-page-details inner-page-padding"> 
+                        <div class="panel-group help-page-wrapper" id="accordion">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <div class="panel-title">
+                                        <a aria-expanded="false" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                                           #  Satın Alma ve Ürün Desteği
+                                        </a>
+                                    </div>
+                                </div>
+                                <div aria-expanded="false" id="collapseOne" role="tabpanel" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                        <h3>Bir Ürün Nasıl Satın Alınır? </h3>
+                                        <p>Integer accumsan efficitur nibh convallis mollis. Sed fringilla sem libero, non dapibus arcu euismod tristique. Sed vitae lobortis nibh. Vivamus tristique gravida augue ut euismod. Aliquam finibus ipsum sem, sit amet volutpat leo pharetra id. Quisque id sollicitudin magna. Nulla velit elit, semper a ligula in, tempus euismod enim. Nullam dignissim ut enim id malesuada. Duis pulvinar pellentesque sagittis. Nullam orci quam, tincidunt eu nibh vitae, mattis volutpat nunc. Nunc rhoncus, libero eget rhoncus consequat, quam justo sodales mi, et lobortis tellus dolor vel diam. Morbi accumsan sem bibendum nibh vehicula tincidunt. Quisque ac auctor dui. Nulla at magna ultrices, finibus mauris at, vulputate nisi. Nullam finibus felis sed quam euismod, id vehicula felis accumsan.</p>
+                                        <h3>Ürün Desteği Nasıl Alınır?</h3>
+                                        <p>Integer accumsan efficitur nibh convallis mollis. Sed fringilla sem libero, non dapibus arcu euismod tristique. Sed vitae lobortis nibh. Vivamus tristique gravida augue ut euismod. Aliquam finibus ipsum sem, sit amet volutpat leo pharetra id. Quisque id sollicitudin magna. Nulla velit elit, semper a ligula in, tempus euismod enim. Nullam dignissim ut enim id malesuada. Duis pulvinar pellentesque sagittis. Nullam orci quam, tincidunt eu nibh vitae, mattis volutpat nunc. Nunc rhoncus, libero eget rhoncus consequat, quam justo sodales mi, et lobortis tellus dolor vel diam. Morbi accumsan sem bibendum nibh vehicula tincidunt. Quisque ac auctor dui. Nulla at magna ultrices, finibus mauris at, vulputate nisi. Nullam finibus felis sed quam euismod, id vehicula felis accumsan. </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <div class="panel-title">
+                                        <a aria-expanded="false" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                                          # Satıcı ve Alıcı Olmak
+                                        </a>
+                                    </div>
+                                </div>
+                                <div aria-expanded="false" id="collapseTwo" role="tabpanel" class="panel-collapse collapse in">
+                                    <div class="panel-body">
+                                         <h3>Bir Ürün Nasıl Satın Alınır? </h3>
+                                        <p>Integer accumsan efficitur nibh convallis mollis. Sed fringilla sem libero, non dapibus arcu euismod tristique. Sed vitae lobortis nibh. Vivamus tristique gravida augue ut euismod. Aliquam finibus ipsum sem, sit amet volutpat leo pharetra id. Quisque id sollicitudin magna. Nulla velit elit, semper a ligula in, tempus euismod enim. Nullam dignissim ut enim id malesuada. Duis pulvinar pellentesque sagittis. Nullam orci quam, tincidunt eu nibh vitae, mattis volutpat nunc. Nunc rhoncus, libero eget rhoncus consequat, quam justo sodales mi, et lobortis tellus dolor vel diam. Morbi accumsan sem bibendum nibh vehicula tincidunt. Quisque ac auctor dui. Nulla at magna ultrices, finibus mauris at, vulputate nisi. Nullam finibus felis sed quam euismod, id vehicula felis accumsan.</p>
+                                        <h3>Ürün Desteği Nasıl Alınır?</h3>
+                                        <p>Integer accumsan efficitur nibh convallis mollis. Sed fringilla sem libero, non dapibus arcu euismod tristique. Sed vitae lobortis nibh. Vivamus tristique gravida augue ut euismod. Aliquam finibus ipsum sem, sit amet volutpat leo pharetra id. Quisque id sollicitudin magna. Nulla velit elit, semper a ligula in, tempus euismod enim. Nullam dignissim ut enim id malesuada. Duis pulvinar pellentesque sagittis. Nullam orci quam, tincidunt eu nibh vitae, mattis volutpat nunc. Nunc rhoncus, libero eget rhoncus consequat, quam justo sodales mi, et lobortis tellus dolor vel diam. Morbi accumsan sem bibendum nibh vehicula tincidunt. Quisque ac auctor dui. Nulla at magna ultrices, finibus mauris at, vulputate nisi. Nullam finibus felis sed quam euismod, id vehicula felis accumsan. </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <div class="panel-title">
+                                        <a aria-expanded="false" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                                           #  Hesabın
+                                        </a>
+                                    </div>
+                                </div>
+                                <div aria-expanded="false" id="collapseThree" role="tabpanel" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                         <h3>Bir Ürün Nasıl Satın Alınır? </h3>
+                                        <p>Integer accumsan efficitur nibh convallis mollis. Sed fringilla sem libero, non dapibus arcu euismod tristique. Sed vitae lobortis nibh. Vivamus tristique gravida augue ut euismod. Aliquam finibus ipsum sem, sit amet volutpat leo pharetra id. Quisque id sollicitudin magna. Nulla velit elit, semper a ligula in, tempus euismod enim. Nullam dignissim ut enim id malesuada. Duis pulvinar pellentesque sagittis. Nullam orci quam, tincidunt eu nibh vitae, mattis volutpat nunc. Nunc rhoncus, libero eget rhoncus consequat, quam justo sodales mi, et lobortis tellus dolor vel diam. Morbi accumsan sem bibendum nibh vehicula tincidunt. Quisque ac auctor dui. Nulla at magna ultrices, finibus mauris at, vulputate nisi. Nullam finibus felis sed quam euismod, id vehicula felis accumsan.</p>
+                                        <h3>Ürün Desteği Nasıl Alınır?</h3>
+                                        <p>Integer accumsan efficitur nibh convallis mollis. Sed fringilla sem libero, non dapibus arcu euismod tristique. Sed vitae lobortis nibh. Vivamus tristique gravida augue ut euismod. Aliquam finibus ipsum sem, sit amet volutpat leo pharetra id. Quisque id sollicitudin magna. Nulla velit elit, semper a ligula in, tempus euismod enim. Nullam dignissim ut enim id malesuada. Duis pulvinar pellentesque sagittis. Nullam orci quam, tincidunt eu nibh vitae, mattis volutpat nunc. Nunc rhoncus, libero eget rhoncus consequat, quam justo sodales mi, et lobortis tellus dolor vel diam. Morbi accumsan sem bibendum nibh vehicula tincidunt. Quisque ac auctor dui. Nulla at magna ultrices, finibus mauris at, vulputate nisi. Nullam finibus felis sed quam euismod, id vehicula felis accumsan. </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <div class="panel-title">
+                                        <a aria-expanded="false" class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
+                                           #  Telif Hakkı ve Ticari Marka
+                                        </a>
+                                    </div>
+                                </div>
+                                <div aria-expanded="false" id="collapseFour" role="tabpanel" class="panel-collapse collapse">
+                                    <div class="panel-body">
+                                         <h3>Bir Ürün Nasıl Satın Alınır? </h3>
+                                        <p>Integer accumsan efficitur nibh convallis mollis. Sed fringilla sem libero, non dapibus arcu euismod tristique. Sed vitae lobortis nibh. Vivamus tristique gravida augue ut euismod. Aliquam finibus ipsum sem, sit amet volutpat leo pharetra id. Quisque id sollicitudin magna. Nulla velit elit, semper a ligula in, tempus euismod enim. Nullam dignissim ut enim id malesuada. Duis pulvinar pellentesque sagittis. Nullam orci quam, tincidunt eu nibh vitae, mattis volutpat nunc. Nunc rhoncus, libero eget rhoncus consequat, quam justo sodales mi, et lobortis tellus dolor vel diam. Morbi accumsan sem bibendum nibh vehicula tincidunt. Quisque ac auctor dui. Nulla at magna ultrices, finibus mauris at, vulputate nisi. Nullam finibus felis sed quam euismod, id vehicula felis accumsan.</p>
+                                        <h3>Ürün Desteği Nasıl Alınır?</h3>
+                                        <p>Integer accumsan efficitur nibh convallis mollis. Sed fringilla sem libero, non dapibus arcu euismod tristique. Sed vitae lobortis nibh. Vivamus tristique gravida augue ut euismod. Aliquam finibus ipsum sem, sit amet volutpat leo pharetra id. Quisque id sollicitudin magna. Nulla velit elit, semper a ligula in, tempus euismod enim. Nullam dignissim ut enim id malesuada. Duis pulvinar pellentesque sagittis. Nullam orci quam, tincidunt eu nibh vitae, mattis volutpat nunc. Nunc rhoncus, libero eget rhoncus consequat, quam justo sodales mi, et lobortis tellus dolor vel diam. Morbi accumsan sem bibendum nibh vehicula tincidunt. Quisque ac auctor dui. Nulla at magna ultrices, finibus mauris at, vulputate nisi. Nullam finibus felis sed quam euismod, id vehicula felis accumsan. </p>
+                                    </div>
+                                </div>
+                            </div>                            
+                        </div>
+                    </div>  
+                </div>  
+            </div> 
+            <!-- Help Page End Here -->
+            <!-- Footer Area Start Here -->
+            <?php require_once 'footer.php' ?>
+            <!-- Footer Area End Here -->
+        </div>
+        <!-- Main Body Area End Here -->
+        <!-- jquery-->  
+        <script src="js\jquery-2.2.4.min.js" type="text/javascript"></script>
+
+        <!-- Plugins js -->
+        <script src="js\plugins.js" type="text/javascript"></script>
+        
+        <!-- Bootstrap js -->
+        <script src="js\bootstrap.min.js" type="text/javascript"></script>
+
+        <!-- WOW JS -->     
+        <script src="js\wow.min.js"></script>
+
+        <!-- Owl Cauosel JS -->
+        <script src="vendor\OwlCarousel\owl.carousel.min.js" type="text/javascript"></script>
+        
+        <!-- Meanmenu Js -->
+        <script src="js\jquery.meanmenu.min.js" type="text/javascript"></script>
+
+        <!-- Srollup js -->
+        <script src="js\jquery.scrollUp.min.js" type="text/javascript"></script>
+
+         <!-- jquery.counterup js -->
+        <script src="js\jquery.counterup.min.js"></script>
+        <script src="js\waypoints.min.js"></script>
+
+        <!-- Nouislider Js -->
+        <script src="vendor\noUiSlider\nouislider.min.js" type="text/javascript"></script>
+
+        <!-- Isotope js -->
+        <script src="js\isotope.pkgd.min.js" type="text/javascript"></script>
+        
+        <!-- Custom Js -->
+        <script src="js\main.js" type="text/javascript"></script>
+
+    </body>
+</html>
